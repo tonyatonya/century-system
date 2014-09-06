a74473c246ad0c54090ce6e75ca9021583b3356d
Athapong Athamasirigun <tonya_0@hotmail.com>
2014-09-06 11:11:19 +0700

    Create movie page

--- headermenu.php
+++
@@ -1,8 +0,0 @@
-<div class="headermenu">
-	<div class="empname">
-		ชื่อพนักงาน : ทดสอบ ทดสอบ
-	</div>
-	<div class="logout">
-	<button type="submit" class="btn btn-primary">เข้าสู่ระบบ</button>
-	</div>
-</div>
--- movie.php
+++ movie.php
@@ -8,17 +8,18 @@
 	<meta name="robots" content="" />
 	<?php require('inc_header.php'); ?>
 </head>
-<body class="withlogo">
+<body class="withlogo movie">
 <div class="container">
 <div class="headermenu">
-	<div class="empname">
-		ชื่อพนักงาน : ทดสอบ ทดสอบ
-	</div>
 	<div class="logout">
-	<button type="submit" class="btn btn-primary">ออกจากระบบ</button>
+		<button type="submit" class="btn btn-primary" onclick="location.href='home.php'">ย้อนกลับหน้าหลัก</button>
 	</div>
 </div>
-
+<div class="mainpanel">
+	<div class="panel panel-1"></div>
+	<div class="panel panel-2"></div>
+	<div class="panel panel-3"></div>
+</div>
 <?php include('footmenu.php') ?>
 </div>
 </body>
