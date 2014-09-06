c8e6eafcae302c5a6c377aeba63eff4d40c8a928
Athapong Athamasirigun <tonya_0@hotmail.com>
2014-09-06 17:58:22 +0700

    Update All Ui and css

--- movie.php
+++ movie.php
@@ -16,16 +16,58 @@
 	</div>
 </div>
 <div class="mainpanel">
-	<div class="panel panel-1"></div>
+	<div class="panel panel-1">
+		<div class="movie-box">
+			<div class="poster">
+				<img src="images/poster.jpg" alt="poster" width="188" height="231">
+			</div>
+		</div>
+		<div class="movie-name">Transforms4_2D_E(G)</div>
+	</div>
 	<div class="panel panel-2">
 		<div class="panel-title">ชื่อภาพยนตร์</div>
 		<div class="movie-list">
 			<ul>
-			<li><a href="#"></a></li>
-		</ul>
+				<li><a href="#">Transforms4_2D_E(G)</a></li>
+				<li><a href="#">Transforms4_2D_E(G)</a></li>
+				<li><a href="#">Transforms4_2D_E(G)</a></li>
+				<li><a href="#">Transforms4_2D_E(G)</a></li>
+				<li><a href="#">Transforms4_2D_E(G)</a></li>
+				<li><a href="#">Transforms4_2D_E(G)</a></li>
+				<li><a href="#">Transforms4_2D_E(G)</a></li>
+				<li><a href="#">Transforms4_2D_E(G)</a></li>
+				<li><a href="#">Transforms4_2D_E(G)</a></li>
+				<li><a href="#">Transforms4_2D_E(G)</a></li>
+				<li><a href="#">Transforms4_2D_E(G)</a></li>
+				<li><a href="#">Transforms4_2D_E(G)</a></li>
+			</ul>
+		</div>
+		<div class="movie-detail">
+			<div class="movie-type">ประเภท : <span>Drama</span></div>
+			<div class="movie-author">ค่ายหนัง <span>Mongkol</span></div>
+			<div class="movie-duration">เวลา : <span>145</span> <span>นาที</span></div>
 		</div>
 	</div>
-	<div class="panel panel-3"></div>
+	<div class="panel panel-3">
+		<div class="panel-title">รอบเวลา</div>
+		<div class="date-time">01/08/2014 10:27:02</div>
+		<div class="time-list">
+			<ul>
+				<li><a href="#"><span class="time">13:00</span> <span class="theater">โรง 3</span></a></li>
+				<li><a href="#"><span class="time">13:00</span> <span class="theater">โรง 3</span></a></li>
+				<li><a href="#"><span class="time">13:00</span> <span class="theater">โรง 3</span></a></li>
+				<li><a href="#"><span class="time">13:00</span> <span class="theater">โรง 3</span></a></li>
+				<li><a href="#"><span class="time">13:00</span> <span class="theater">โรง 3</span></a></li>
+				<li><a href="#"><span class="time">13:00</span> <span class="theater">โรง 3</span></a></li>
+				<li><a href="#"><span class="time">13:00</span> <span class="theater">โรง 3</span></a></li>
+				<li><a href="#"><span class="time">13:00</span> <span class="theater">โรง 3</span></a></li>
+				<li><a href="#"><span class="time">13:00</span> <span class="theater">โรง 3</span></a></li>
+				<li><a href="#"><span class="time">13:00</span> <span class="theater">โรง 3</span></a></li>
+				<li><a href="#"><span class="time">13:00</span> <span class="theater">โรง 3</span></a></li>
+				<li><a href="#"><span class="time">13:00</span> <span class="theater">โรง 3</span></a></li>
+			</ul>
+		</div>
+	</div>
 </div>
 <?php include('footmenu.php') ?>
 </div>
---
+++ theater.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+  <meta charset="utf-8">
+	<title></title>
+	<meta name="description" content="" />
+  	<meta name="keywords" content="" />
+	<meta name="robots" content="" />
+	<?php require('inc_header.php'); ?>
+</head>
+<body class="withlogo theater">
+<div class="container">
+<div class="headermenu">
+	<div class="logout">
+		<button type="submit" class="btn btn-primary" onclick="location.href='home.php'">ย้อนกลับหน้าหลัก</button>
+	</div>
+</div>
+<div class="mainpanel">
+	<div class="theater-panel">
+		<div class="theater-num">โรงที่ <span>1</span></div>
+		<div class="theater-movie">
+			<ul>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="theater-panel">
+		<div class="theater-num">โรงที่ <span>1</span></div>
+		<div class="theater-movie">
+			<ul>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="theater-panel">
+		<div class="theater-num">โรงที่ <span>1</span></div>
+		<div class="theater-movie">
+			<ul>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="theater-panel">
+		<div class="theater-num">โรงที่ <span>1</span></div>
+		<div class="theater-movie">
+			<ul>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="theater-panel">
+		<div class="theater-num">โรงที่ <span>1</span></div>
+		<div class="theater-movie">
+			<ul>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="theater-panel">
+		<div class="theater-num">โรงที่ <span>1</span></div>
+		<div class="theater-movie">
+			<ul>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="theater-panel">
+		<div class="theater-num">โรงที่ <span>1</span></div>
+		<div class="theater-movie">
+			<ul>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="theater-panel">
+		<div class="theater-num">โรงที่ <span>1</span></div>
+		<div class="theater-movie">
+			<ul>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+				<li><a href="#">13:00-Transforms4_2D_E(G)</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+<?php include('footmenu.php') ?>
+</div>
+</body>
+</html>
